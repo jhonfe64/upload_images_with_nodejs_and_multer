4e889fd4acbbe41163e53f8e2687daa3ab959eb0
John Pérez <jhonfe648@gmail.com>
2020-09-27 00:20:09 -0500

    limpiando el codigo

--- src/index.js
+++ src/index.js
@@ -14,26 +14,14 @@ app.set('views', path.join(__dirname, 'views'));
 
 const storage = multer.diskStorage({
     destination: path.join(__dirname, 'public/uploads'),
-    //file name me prmite configurar el nombre de las imagenes que suba, es una funcion que recibe un callback, el req, el archivo file y un callback
     filename: (req, file, cb) => {
-        //como en todos los callbacks se llaan dentro de la funcion, no se quiere enviar nada por el req, entonces ponemos null, del file enviamos solo el originalname
-        //cb(null, file.originalname);
         cb(null, uuidv4() + path.extname(file.originalname).toLocaleLowerCase());
     }
-    //le pasamos la variable storage a multer
 })
 
-//Middlewares 
 
-//multer es un middleware por eso va antes de las rutas, osea se aplica para todas las rutas
-
-//multer como parametrun objeto
-//usamos el metodo single que recibe el name del input tipo file desde donde se va a subir la imagen
-//el metodo single permite subir una imagen a la vezz
 app.use(multer({
     storage: storage,
-    //las imagenes se van a guardar en esta carpeta
-    //la imagen se guardara en req.file que se pedra desde la ruta
     dest: path.join(__dirname, 'public/uploads'),
     limits: {
         fileSize: 1000000000000000000
--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -8,8 +8,6 @@ router.get('/', (req, res)=>{
 //==> recibe la info de la imagen al hacer click en el btn
 router.post('/upload', (req, res)=>{
     res.send('uploaded');
-    //atravez de req.file obtenemos los datos de la imagen que estamos subiendo, ojo el nombre de la imagen es un id unico para cada uan de lasmimagenes, ahora queremos que el nombre sea el nombre origial de la imagen y la extension original
-    //arriba creamos la constante storage
     console.log(req.file);
 });
 
